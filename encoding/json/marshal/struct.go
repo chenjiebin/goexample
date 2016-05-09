4d9cc35c4758892be5afd205b85bf85d345d32b1
chenjiebin <sjlinyu@qq.com>
2016-05-09 10:35:58 +0800

    json编码调整

--- encoding/json/marshal/struct.go
+++ encoding/json/marshal/struct.go
@@ -1,4 +1,5 @@
 // json编码
+// 从struct类型编码成json格式字符串
 package main
 
 import (
@@ -7,12 +8,14 @@ import (
 )
 
 func main() {
+	// 声明一个结构体
 	type ColorGroup struct {
 		ID     int      `json:"id,string"`
 		Name   string   `json:"name,omitempty"`
 		Colors []string `json:"colors"`
 	}
 
+	// 对ColorGroup类型的变量进行编码
 	group := ColorGroup{
 		ID:     1,
 		Name:   "Reds",
@@ -23,8 +26,9 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(string(b))
+	// output: {"id":"1","name":"Reds","colors":["Crimson","Red","Ruby","Maroon"]}
 
-	// 如果没有设置Name属性值，则在编码成json的时候会忽略Name属性
+	// 如果没有设置Name属性值，因为标记为了omitempty属性，则在编码成json的时候会忽略Name属性
 	group = ColorGroup{
 		ID:     1,
 		Colors: []string{"Crimson", "Red", "Ruby", "Maroon"},
@@ -34,6 +38,7 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(string(b))
+	// output: {"id":"1","colors":["Crimson","Red","Ruby","Maroon"]}
 
 	// 如果没有设置Colors值，因为没有omitempty属性，会输出nil
 	group = ColorGroup{
@@ -45,5 +50,6 @@ func main() {
 		panic(err)
 	}
 	fmt.Println(string(b))
+	// output: {"id":"1","name":"Reds","colors":null}
 
 }
