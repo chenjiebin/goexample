906cbbcfb7117aac37f1665cbf2d4396a2dd6b6a
chenjiebin <303410541@qq.com>
2019-11-15 09:06:38 +0800

    文档压缩

---
+++ archive/tar/compress.go
@@ -0,0 +1,52 @@
+// tar压缩
+// 这边演示一下从源文件进行压缩，然后输出一个tar.gz文档
+// 作者：陈杰斌
+// 参考地址：http://www.01happy.com/golang-tar/
+package main
+
+import (
+	"archive/tar"
+	"bytes"
+	"log"
+	"os"
+)
+
+func main() {
+	// 创建一个缓冲区用来保存压缩文件内容
+	var buf bytes.Buffer
+	// 创建一个压缩文档
+	tw := tar.NewWriter(&buf)
+
+	// 定义一堆文件
+	// 将文件写入到压缩文档tw
+	var files = []struct {
+		Name, Body string
+	}{
+		{"readme.txt", "This archive contains some text files."},
+		{"gopher.txt", "Gopher names:\nGeorge\nGeoffrey\nGonzo"},
+		{"todo.txt", "Get animal handling license."},
+	}
+	for _, file := range files {
+		hdr := &tar.Header{
+			Name: file.Name,
+			Mode: 0600,
+			Size: int64(len(file.Body)),
+		}
+		if err := tw.WriteHeader(hdr); err != nil {
+			log.Fatal(err)
+		}
+		if _, err := tw.Write([]byte(file.Body)); err != nil {
+			log.Fatal(err)
+		}
+	}
+	if err := tw.Close(); err != nil {
+		log.Fatal(err)
+	}
+
+	// 将压缩文档内容写入文件 file.tar.gz
+	f, err := os.OpenFile("file.tar.gz", os.O_CREATE|os.O_WRONLY, 0666)
+	if err != nil {
+		log.Fatal(err)
+	}
+	buf.WriteTo(f)
+}
